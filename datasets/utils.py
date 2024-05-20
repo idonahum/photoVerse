72df40849c36cd8d68cf30d008a6ae0b49496f97
idonahum1 <ido.nahum@traxretail.com>
2024-05-20 19:08:53 +0300

    test

--- datasets/utils.py
+++ datasets/utils.py
@@ -221,3 +221,31 @@ def random_batch_slicing(example, batch_size, num_of_samples):
             sliced_batch[key] = value
     return sliced_batch
 
+
+def create_test_train_from_known_list(train_list_file, test_list_file, src_folder, dest_folder, force_copy=False):
+    make_folder(dest_folder)
+    make_folder(os.path.join(dest_folder, 'train'))
+    make_folder(os.path.join(dest_folder, 'test'))
+    train_list = open(train_list_file, 'r').read().splitlines()
+    test_list = open(test_list_file, 'r').read().splitlines()
+
+    for img in train_list:
+        src_img = os.path.join(src_folder, img)
+        dest_img = os.path.join(dest_folder, 'train', img)
+        if not os.path.exists(dest_img) or force_copy:
+            shutil.copy(src_img, dest_img)
+
+    for img in test_list:
+        src_img = os.path.join(src_folder, img)
+        dest_img = os.path.join(dest_folder, 'test', img)
+        if not os.path.exists(dest_img) or force_copy:
+            shutil.copy(src_img, dest_img)
+
+    return os.path.join(dest_folder, 'train'), os.path.join(dest_folder, 'test')
+
+if __name__ == "__main__":
+    src_folder = ''
+    train_list_file = ''
+    test_list_file = ''
+    dest_folder = ''
+    create_test_train_from_known_list(train_list_file, test_list_file, src_folder, dest_folder)
---
+++ test.py
@@ -0,0 +1,128 @@
+import argparse
+
+import torch
+
+from datasets.custom import CustomDataset, collate_fn
+from models.infer import run_inference
+from models.loss import FaceLoss
+from models.modeling_utils import load_models
+from utils.image_utils import to_pil, denormalize
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Simple example of a training script.")
+    parser.add_argument(
+        "--pretrained_model_name_or_path",
+        type=str,
+        default="runwayml/stable-diffusion-v1-5",
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+    parser.add_argument(
+        "--pretrained_photoverse_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to pretrained ip adapter model. If not specified weights are initialized randomly.",
+    )
+    parser.add_argument(
+        "--data_root_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Training datasets root path",
+    )
+
+    parser.add_argument(
+        "--img_subfolder",
+        type=str,
+        default="images",
+        help="Subfolder relative to data_root_path containing images",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="results",
+        help="The output directory where the model predictions and checkpoints will be written.",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=16, help="Batch size (per device) for the testing dataloader."
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        default=4,
+        help=(
+            "Number of subprocesses to use for datasets loading. 0 means that the datasets will be loaded in the main process."
+        ),
+    )
+    parser.add_argument(
+        '--denoise_timesteps',
+        type=int,
+        default=10,
+        help='Number of timesteps for inference'
+    )
+
+    parser.add_argument(
+        '--guidance_scale',
+        type=float,
+        default=2.0,
+        help='Guidance scale for inference'
+    )
+    parser.add_argument(
+        '--device',
+        type=str,
+        default="cuda" if torch.cuda.is_available() else "cpu",
+        help="Device (cuda or cpu)"
+    )
+    parser.add_argument("--extra_num_tokens", type=int, default=4, help="Number of additional tokens")
+    parser.add_argument(
+        "--image_encoder_layers_idx",
+        type=list,
+        default=[4, 8, 12, 16],
+        help="Image encoder extra layers indices to use as tokens for the text encoder, should be equal to extra_num_tokens",
+    )
+    parser.add_argument(
+        "--resolution",
+        type=int,
+        default=512,
+        help=(
+            "The resolution for input images"
+        ),
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    face_similarity = FaceLoss(device=args.device, model_name='arcface')
+    tokenizer, text_encoder, vae, unet, image_encoder, image_adapter, text_adapter, scheduler, _ = load_models(
+        args.pretrained_model_name_or_path, args.extra_num_tokens, args.pretrained_photoverse_path)
+
+    test_dataset = CustomDataset(data_root=args.data_root_path, img_subfolder=args.img_subfolder,
+                  tokenizer=tokenizer, size=args.resolution)
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        shuffle=True,
+        collate_fn=collate_fn,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+    )
+    image_encoder_layers_idx = torch.tensor(args.image_encoder_layers_idx).to(args.device)
+    similarity_list = []
+    for sample in test_dataloader:
+        with torch.no_grad():
+            pixel_values = sample["pixel_values"].to(args.device)
+            gen_tensors = run_inference(sample, tokenizer, image_encoder, text_encoder, unet, text_adapter,
+                                        image_adapter, vae,
+                                        scheduler, args.device, image_encoder_layers_idx,
+                                        guidance_scale=args.guidance_scale,
+                                        timesteps=args.denoise_timesteps, token_index=0)
+            similarity_metric = face_similarity(pixel_values, gen_tensors, normalize=False,
+                                          maximize=False).detach().item()
+            similarity_list.append(similarity_metric)
+            generated_images = [to_pil(denormalize(img)) for img in generated_images]
+    print(f"Num of timesteps: {args.denoise_timesteps}")
+    print(f"Average similarity: {sum(similarity_list)/len(similarity_list)}")
+
+if __name__ == "__main__":
+    main()
